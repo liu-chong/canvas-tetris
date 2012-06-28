c6fb3dc5a735de4e1dd858f51d67a0245e60adb7
liu-chong <liuchong14@gmail.com>
2012-06-28 12:55:08 +0800

    Add key space -> drop

--- js/controller.js
+++ js/controller.js
@@ -1,12 +1,14 @@
-document.body.onkeydown = function( e ) {
-    var keys = {
-        37: 'left',
-        39: 'right',
-        40: 'down',
-        38: 'rotate'
-    };
-    if ( typeof keys[ e.keyCode ] != 'undefined' ) {
-        keyPress( keys[ e.keyCode ] );
-        render();
-    }
+document.body.onkeydown = function(e) {
+  console.log(e.keyCode);
+  var keys = {
+      32: "drop",
+      37: 'left',
+      39: 'right',
+      40: 'down',
+      38: 'rotate'
+  };
+  if (typeof keys[e.keyCode] != 'undefined') {
+    keyPress(keys[e.keyCode]);
+    render();
+  }
 };
